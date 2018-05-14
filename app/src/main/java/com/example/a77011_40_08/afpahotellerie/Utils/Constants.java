1324e40efd7140a87a094ad2996c92500bbd9bd9
Pierre <pierre@gmail.com>
2018-05-14 17:08:40 +0200

    1er commit initial du projet d'hotellerie

---
+++ app/src/main/java/com/example/a77011_40_08/afpahotellerie/Utils/Constants.java
@@ -0,0 +1,24 @@
+package com.example.a77011_40_08.afpahotellerie.Utils;
+
+/**
+ * Created by 77011-40-05 on 14/03/2018.
+ */
+
+public class Constants {
+
+
+
+    //GENERAL
+    public static final String SHARED_PREFERENCE = "ShareData" ;
+    public static final String _TAG_LOG = "#AfpaHotellerie";
+    public static final String _ERROR_PHP = "Erreur PHP";
+
+    //FRAGMENT CODE -->HomeActivity
+    public final static int _FRAG_HOME = 0;
+
+
+
+    public static final String APP_NAME = "AfpaHotellerie";
+    public static final String _URL_WEBSERVICE =  "http://10.75.25.37:8080/AfpaHotellerie/";
+
+}
